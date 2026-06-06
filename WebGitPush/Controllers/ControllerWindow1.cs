dec55bc81a1cdaecaa4b71d772871e9ced4e6bce
Loreson <log.ivan.shirinkin@gmail.com>
2026-06-06 21:54:38 +0500

    Рабочий main

---
+++ WebGitPush/Controllers/ControllerWindow1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebGitPush.Controllers
+{
+    public class ControllerWindow1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebGitPush/Controllers/MainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebGitPush.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
